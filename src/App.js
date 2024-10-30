c216bb11f36e906b60cda4c0ed19e19e2adf944d
biou <biou1204@naver.com>
2024-10-30 20:46:35 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +17,10 @@ function App() {
         >
           Learn React
         </a>
+         */}
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        root.render(<h1>Hello, world!</h1>);
+
       </header>
     </div>
   );
