ee95b774b3ec37946354fd9b4827c7176ad1d341
biou <biou1204@naver.com>
2024-11-18 22:04:11 +0900

    일정 화면 추가 및 api연동 성공, 아코디언 오류

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import SplashScreen from './pages/splash_screen/splash_screen.js';
 import MainScreen from './pages/main_screen/main_screen.js';
 import CreatePlan from './pages/create_plan_screen/create_plan.js'
 import ChoiceTag from'./pages/choice_tag_screen/choice_tag.js'
+import Loading from './pages/loading_screen/loading.js';
+import MyPlan from './pages/myplan_screen/myplan.js';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/main" element={<MainScreen />} />
           <Route path="/createplan" element={<CreatePlan />} />
           <Route path="/choicetag" element={<ChoiceTag />} />
+          <Route path="/loading" element={<Loading />} />
+          <Route path="/myplan" element={<MyPlan />} />
         </Routes>
       </div>
     </Router>
@@ -23,3 +27,4 @@ function App() {
 }
 
 export default App;
+
